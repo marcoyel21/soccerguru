452d77e737d4c0e8c2fa1b3a7a9e4e566dfa7928
Marco Ramos <57338890+marcoyel21@users.noreply.github.com>
2022-10-13 23:07:48 -0500

    webhook discord

--- model/dag_train.py
+++ model/dag_train.py
@@ -2,6 +2,7 @@ from datetime import timedelta
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.utils.dates import days_ago
+from airflow.providers.discord.operators.discord_webhook import DiscordWebhookOperator
 
 import os
 
@@ -22,6 +23,20 @@ dag = DAG(
     schedule_interval='15 13 * * 5',
     start_date = days_ago(1),
     tags=["football"])
+with DAG(
+    'process_2_train',
+    description = 'Model training',
+    #At 13:15 on day-of-month 1.
+    schedule_interval='15 13 * * 5',
+    start_date = days_ago(1),
+    tags=["football"],
+) as dag:
+
+    alert = DiscordWebhookOperator(
+        task_id= "discord_alert_start",
+        http_conn_id = 'discord',
+        webhook_endpoint ='webhooks/1030306654106951731/5MHkAQZMKDMUn30n1HjL-BHtDSVU5QkQFK7sZQmBXVhWtK4I-SzI97E0g2u85gjVzuNS', 
+        message = 'DAG Train started succesfully',)
 
 t1 = BashOperator(
     task_id='get_latest_model',
@@ -45,5 +60,14 @@ t3 = BashOperator(
     bash_command='{{params.path_upload_model}} ',
     dag=dag)
 
+alert2 = DiscordWebhookOperator(
+    task_id= "discord_alert_finish",
+    http_conn_id = 'discord',
+    webhook_endpoint ='token', 
+    message = 'DAG Train finished succesfully',
+    dag=dag)
+
+
 t1 >> t2
 t2 >> t3
+t3 >> alert2
