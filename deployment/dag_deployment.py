e51fe119667564f68bb118d0a46c203e2d4fabe9
Marco Ramos <57338890+marcoyel21@users.noreply.github.com>
2022-11-04 02:05:52 -0600

    Update dag_deployment.py

--- deployment/dag_deployment.py
+++ deployment/dag_deployment.py
@@ -42,7 +42,7 @@ t2 = BashOperator(
     task_id='html_to_bucket',
     depends_on_past=False,
     params=params,
-    bash_command='gsutil cp /home/marcoyel21/airflow/dags/soccerguru/deployment/index.html gs://bets_backend/',
+    bash_command='{{params.path_html_to_bucket}} ',
     dag=dag)
 
 alert2 = DiscordWebhookOperator(
